2c1406f7a592872a9120026bd9e2a190a1341135
vu giap <vudinhgiap11@gmail.com>
2023-07-22 09:34:57 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace doituonghinhhoc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape shape = new Shape();
+
+            Console.WriteLine(shape);
+
+            shape = new Shape("red", false);
+
+            Console.WriteLine(shape);
+
+            Console.WriteLine("hello world");
+
+            Circle circle = new Circle();
+
+            Console.WriteLine(circle);
+
+            circle = new Circle(3.5);
+
+            Console.WriteLine(circle);
+
+            circle = new Circle(3.5, "ngoc", false);
+
+            Console.WriteLine(circle);
+            Rectangle rectangle = new Rectangle();
+            Console.WriteLine(rectangle);
+
+            rectangle = new Rectangle(2.3, 5.8);
+            Console.WriteLine(rectangle);
+
+            rectangle = new Rectangle(2.5, 3.8, "orange", true);
+            Console.WriteLine(rectangle);
+            Square square = new Square();
+            Console.WriteLine(square);
+
+            square = new Square(2.3);
+            Console.WriteLine(square);
+
+            square = new Square(5.8, "yellow", true);
+            Console.WriteLine(square);
+        }
+    }
+}
---
+++ Shape.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace doituonghinhhoc
+{
+    public class Shape
+    {
+        private string color = "green";
+        private bool filled = true;
+
+        public Shape()
+        {
+
+        }
+        public Shape(string color, bool filled)
+        {
+            this.color = color;
+            this.Filled = filled;
+        }
+
+        public string Color { get => color; set => color = value; }
+        public bool Filled { get => filled; set => filled = value; }
+        private bool isFilled()
+        {
+            return filled;
+        }
+
+        public override string ToString()
+        {
+            return "a shape with color of "
+                   + color
+                   +  " and " 
+                   + (isFilled()? "filled" : "not filled");
+        }
+
+
+    }
+    public class Circle : Shape
+    {
+        private double radius = 1.0;
+        public Circle()
+        {
+
+        }
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+        public Circle(double radius, string color, bool filled) : base(color, filled)
+        {
+            this.radius = radius;
+        }
+
+        public double Getradius()
+        {
+            return radius;
+        }
+        public void  Setradius(double radius)
+        {
+            this.radius = radius;
+        }
+        public double GetArea()
+        {
+            return radius * radius * Math.PI;
+        }
+        public double GetPerimeter()
+        {
+            return 2*radius * Math.PI;
+        }
+        public override string ToString()
+        {
+            return " a circle with radius = " + Getradius() + " which is a subclass of " + base.ToString();
+        }
+    }
+    public class Rectangle : Shape
+    {
+        private double width = 1.0;
+        private double length = 1.0;
+
+        public Rectangle()
+        {
+
+        }
+        public Rectangle(double width, double length)
+        {
+            this.width = width;
+            this.length = length;
+        }
+        public Rectangle(double width, double length, string color, bool filled) : base(color, filled)
+        {
+            this.width = width;
+            this.length = length;
+        }
+        public double GetWidth()
+        {
+            return width;
+        }
+        public virtual void SetWidth(double width)
+        {
+            this.width = width;
+        }
+        public double GetLength()
+        {
+            return length;
+        }
+        public virtual void SetLength(double length)
+        {
+            this.length= length;
+        }
+        public double GetArea()
+        {
+            return width* length;
+        }
+        public double Getperimeter()
+        {
+            return 2 *(width + this.length);
+        }
+        public override string ToString()
+        {
+            return "A rectangle with width = " + GetWidth() + " and length = " + GetLength() + ", which is a subclass of" + base.ToString();
+        }
+    }
+    public class Square : Rectangle
+    {
+        public Square()
+        {
+
+        }
+        public Square(double side) : base(side, side)
+        {
+
+        }
+        public Square(double side, string color, bool filled): base(side,side, color, filled)
+        { 
+        }
+        public double GetSide()
+        {
+            return GetWidth();
+        }
+        public void SetSide(double side)
+        {
+            SetWidth(side);
+            SetLength(side);
+        }
+        public override void SetWidth(double width)
+
+        {
+
+            SetSide(width);
+
+        }
+
+        public override void SetLength(double length)
+
+        {
+
+            SetSide(length);
+
+        }
+
+        public override String ToString()
+        {
+            return "a awuare with side = "
+                +GetSide()
+                + ",which is a subcless of " + base.ToString();
+          
+        }
+    }
+}
